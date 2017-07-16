2813494f363e832ec86d07f0abe4249e8ba38778
Orion Wolf_Hubbard <owolfhu1@gmail.com>
2017-07-16 10:20:23 -0400

    for heroku

--- src/main/java/com/example/demo/controller/SnippitController.java
+++ src/main/java/com/example/demo/controller/SnippitController.java
@@ -58,10 +58,8 @@ public class SnippitController {
         if (snippet.getTitle().length() > 0 && snippet.getLanguages().length() == 0)
             snippets = snippetRepository.findAllByTitleContaining(snippet.getTitle());
 
-
         else if (snippet.getLanguages().length() > 0 && snippet.getTitle().length() == 0)
             snippets = snippetRepository.findAllByLanguagesContaining(snippet.getLanguages());
-
 
         else if (snippet.getTitle().length() > 0 && snippet.getLanguages().length() > 0) {
             snippets = snippetRepository.findAllByTitleContaining(snippet.getTitle());
